06545db0234de5318a222aa1b038b67c737427c9
Karma Tamang <tamangkarma160@gmail.com>
2025-06-08 12:24:34 +0545

    feat: new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 className='bg-red-500 text-center'>hello this is me karma tamang</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
