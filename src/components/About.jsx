02ebef44d13a5c0d2c3cbe4bc39a86c19419c9e5
Karma Tamang <tamangkarma160@gmail.com>
2025-06-25 11:41:01 +0545

    updated

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>We are a travel company dedicated to providing the best experiences in Nepal.</p>
+    </>
+  )
+}
+
+export default About
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import HeroBanner from "../components/HeroBanner";
 import CookieConsent from "../components/Cookies";
 import Navbar from "../components/Navbar";
+import About from "../components/About";
 
 const Homepage = () => {
   return (
     <>
       <Navbar/>
       <HeroBanner />
+      <About />
       <CookieConsent />
     </>
   );
