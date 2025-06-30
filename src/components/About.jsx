c3672f556da861ab5ea47b4bee767015716b8a61
Karma Tamang <tamangkarma160@gmail.com>
2025-06-30 20:55:08 +0545

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,8 @@ const About = () => {
   return (
     <>
       <h1>About Us</h1>
-      <p>We are a travel company dedicated to providing the best experiences in Nepal.</p>
-      <p>Our team consists of experienced guides and travel enthusiasts who are passionate about sharing the beauty of Nepal with the world.</p>
-      <p>We offer a variety of tours and packages to suit every traveler's needs, from trekking in the Himalayas to cultural tours in the Kathmandu Valley.</p>
-      <p>Our mission is to create unforgettable memories for our clients while promoting sustainable tourism practices in Nepal.</p>
-      <p>Join us on an adventure of a lifetime!</p>
-      <h1>This is all</h1>
-      <p>Okay this is just a</p>
+      <p>We are a team of passionate developers dedicated to building amazing web applications.</p>
+      <p>Our mission is to create user-friendly and efficient software solutions that meet the needs of our clients.</p>
     </>
   )
 }
