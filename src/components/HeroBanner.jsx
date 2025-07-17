f5e67089679b316ed81260974bbf79af8f3a38e3
Karma Tamang <tamangkarma160@gmail.com>
2025-07-17 22:49:18 +0545

    updated

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -11,17 +11,17 @@ const HeroBanner = () => {
       />
       <div className="absolute inset-0 bg-black/60 flex items-center justify-center gap-48 px-4">
         <div className="text-center text-white">
-          <h1 className="text-4xl md:text-6xl font-bold drop-shadow-md">
+          <h1 className="text-xl  md:text-6xl font-bold drop-shadow-md">
             Hamro Yatra Nepal
           </h1>
-          <p className="mt-4 text-lg md:text-2xl font-medium italic">
+          <p className="mt-4 text-lg md:text-2xl font-medium italic ">
             Discover Nepal, Live the Journey
           </p>
           <button className="mt-8 bg-white text-black px-6 py-3 rounded-full text-sm md:text-base font-semibold shadow-md hover:bg-gray-100 transition">
             Explore Destinations
           </button>
         </div>
-        <div className="w-72">
+        <div className="">
           <img src={sideImg} alt=""
           className="rounded-3xl h-[23rem]"
           />
