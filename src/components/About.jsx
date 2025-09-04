05bc1ab05eadc3eb91ffa314f31d53d09a515a72
Karma Tamang <tamangkarma160@gmail.com>
2025-09-04 19:35:13 +0545

    chore: about page updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <>
+    <div className='p-8 max-w-4xl mx-auto'>
+
       <h1>About Us</h1>
       <p>We are a team of passionate developers dedicated to building amazing web applications.</p>
-      <p>Our mission is to create user-friendly and efficient software solutions that meet the needs of our clients.</p>
-      <h1>We all are</h1>
+      <p>Our mission is to create user-friendly and efficient solutions that meet the needs of our clients.</p>
+    </div>
     </>
   )
 }
