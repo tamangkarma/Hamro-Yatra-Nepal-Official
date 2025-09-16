6264db76ee0acb1c455a471484684d44506367ad
Karma Tamang <tamangkarma160@gmail.com>
2025-09-16 19:04:58 +0545

    chore: updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,6 @@ const About = () => {
     <>
     <div className='p-8 max-w-4xl mx-auto'>
       <h1>About Us</h1>
-      <p>Our mission is to create user-friendly and efficient solutions that meet the needs of our clients.</p>
     </div>
     </>
   )
